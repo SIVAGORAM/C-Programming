c6723b8e8b80cec5075e058cf9a16d28f2b32b07
SIVA GORAM <108587685+SIVAGORAM@users.noreply.github.com>
2024-09-20 10:44:16 +0530

    Update comments.cpp
    Comments In Cpp Programming.

--- 1. CPP Basic/comments.cpp
+++ 1. CPP Basic/comments.cpp
@@ -1,15 +1,27 @@
-#include<iostream>
+#include<iostream>  // This line includes the iostream library, which allows us to use input and output functions like cout.
 
-using namespace std;
+using namespace std; // This allows us to use the standard namespace, so we don’t have to prefix standard functions with std::.
 
-int main(){
-    cout<< "Hello Coders";
-    // This is a single line comment
-    cout<<"Hello Siva Welcome to Coding";
-    /* This is a 
-    Multiline line comment 
-    using in CPP Programming
+int main() {
+    // This is a single-line comment.
+    // The next line will print "Hello Coders" to the console.
+    cout << "Hello Coders" << endl;
+
+    // Another single-line comment:
+    // The next line will print "Hello Siva Welcome to Coding".
+    cout << "Hello Siva Welcome to Coding" << endl;
+
+    /* 
+    This is a multi-line comment.
+    Multi-line comments can span several lines.
+    They are useful when we need to add detailed explanations
+    or temporarily disable a block of code.
     */
-    return 0;
 
+    /*
+    cout << "This line of code is commented out and won't run!";
+    */
+
+    // The program returns 0, indicating that it finished successfully.
+    return 0; 
 }
