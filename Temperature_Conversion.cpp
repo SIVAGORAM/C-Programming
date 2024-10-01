d235afc981431fa6f13be08d440d3e4075ade957
SIVA GORAM <108587685+SIVAGORAM@users.noreply.github.com>
2024-10-01 11:18:51 +0530

    Update Temperature_Conversion.cpp

--- Temperature_Conversion.cpp
+++ Temperature_Conversion.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float celsius, fahrenheit;
+
+    // Convert Celsius to Fahrenheit
+    cout << "Enter temperature in Celsius: ";
+    cin >> celsius;
+    fahrenheit = (celsius * 9/5) + 32;
+    cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+
+    // Convert Fahrenheit to Celsius
+    cout << "Enter temperature in Fahrenheit: ";
+    cin >> fahrenheit;
+    celsius = (fahrenheit - 32) * 5/9;
+    cout << "Temperature in Celsius: " << celsius << endl;
+
+    return 0;
+}
 
