b482de4a44456e4e0a2b0d69cca041ba38de8537
SIVA GORAM <108587685+SIVAGORAM@users.noreply.github.com>
2024-10-01 11:17:23 +0530

    Update Macros_Type_Range_Macros_Problem.cpp

--- Macros_Type_Range_Macros_Problem.cpp
+++ Macros_Type_Range_Macros_Problem.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <climits>  // For type range macros
+using namespace std;
+
+// Define macro for calculating square of a number
+#define SQUARE(x) ((x) * (x))
+
+int main() {
+    // Using the SQUARE macro
+    int num = 5;
+    cout << "Square of " << num << " is: " << SQUARE(num) << endl;
+
+    // Using type range macros from <climits>
+    cout << "The maximum value of int: " << INT_MAX << endl;
+    cout << "The minimum value of int: " << INT_MIN << endl;
+    cout << "The maximum value of char: " << CHAR_MAX << endl;
+    cout << "The minimum value of char: " << CHAR_MIN << endl;
+
+    return 0;
+}
 
